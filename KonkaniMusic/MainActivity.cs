63d76766333e73d1e794c9ab68a757fb6c5132fc
Vijayshree Softwares <Vijayshree Softwares@DESKTOP-B0MJDNR>
2017-04-14 17:11:58 +0530

    Add project files.

---
+++ KonkaniMusic/MainActivity.cs
@@ -0,0 +1,282 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Text.Method;
+using Cheesebaron.SlidingUpPanel;
+using Android.Util;
+using Android.Support.V7.App;
+using Android.Media;
+using static Android.Media.MediaPlayer;
+using System;
+using Android.Support.V7.Widget;
+
+namespace KonkaniMusic
+{
+    [Activity(Label = "KonkaniMusic", MainLauncher = true, Icon = "@drawable/music",
+        Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        public SeekBar seekbar;
+        MediaPlayer mp;
+        bool playing = false;
+        private const string SavedStateActionBarHidden = "saved_state_action_bar_hidden";
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+            var layout = FindViewById<SlidingUpPanelLayout>(Resource.Id.sliding_layout);
+            var playbt = FindViewById<ImageButton>(Resource.Id.bt_play);
+            var playbtn = FindViewById<ImageButton>(Resource.Id.play);
+            var queuebtn = FindViewById(Resource.Id.queue);
+            var txtSongName = FindViewById<TextView>(Resource.Id.SongNameText);
+            var txtArtistName = FindViewById<TextView>(Resource.Id.ArtistNameText);
+
+
+            var maintext = FindViewById<TextView>(Resource.Id.mainFeatured);
+
+            maintext.Text = "Featured songs: ";
+            mPhotoAlbum = new PhotoAlbum();
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerViewFeatured);
+            mLayoutManager = new LinearLayoutManager(this, 0, false);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+            mAdapter.ItemClick += OnItemClick;
+            mRecyclerView.SetAdapter(mAdapter);
+
+            maintext = FindViewById<TextView>(Resource.Id.mainNew);
+
+            maintext.Text = "New songs: ";
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerViewNew);
+            mLayoutManager = new LinearLayoutManager(this, 0, false);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+            mAdapter.ItemClick += OnItemClick;
+            mRecyclerView.SetAdapter(mAdapter);
+
+            maintext = FindViewById<TextView>(Resource.Id.mainDownloaded);
+
+            maintext.Text = "Downloaded songs: ";
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerViewDownloaded);
+            mLayoutManager = new LinearLayoutManager(this, 0, false);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+            mAdapter = new PhotoAlbumAdapter(mPhotoAlbum);
+            mAdapter.ItemClick += OnItemClick;
+            mRecyclerView.SetAdapter(mAdapter);
+
+            seekbar = FindViewById<SeekBar>(Resource.Id.songProgressBar);
+
+
+            mp = new MediaPlayer();
+            mp.BufferingUpdate += updateSeekbar;
+            mp.SetAudioStreamType(Stream.Music);
+            mp.SetWakeMode(ApplicationContext, WakeLockFlags.Partial);
+
+            mp.Prepared += (sender, args) =>
+            {
+                System.Diagnostics.Debug.WriteLine("called prepare function :)" + mp.Duration);
+                seekbar.Max = mp.Duration;
+                //seekbar_top.Max = mp.Duration;
+                mp.Start();
+                System.Diagnostics.Debug.WriteLine("called prepare function here only:)" + mp.Duration);
+                updateProgressBar();
+            };
+            mp.Completion += (sender, args) => StopMusic();
+
+            playbtn.Click += (s, e) => {
+                System.Diagnostics.Debug.WriteLine("play clicked");
+                if (!playing)
+                {
+                    PlayMusic();
+                    playing = true;
+                    playbtn.SetImageResource(Resource.Mipmap.pause);
+                    playbt.SetImageResource(Resource.Mipmap.pause);
+
+                }
+                else
+                {
+                    PauseMusic();
+                    playing = false;
+                    playbtn.SetImageResource(Resource.Mipmap.playy);
+                    playbt.SetImageResource(Resource.Mipmap.playy);
+
+                }
+                Toast.MakeText(ApplicationContext, "play clicked", ToastLength.Short).Show();
+            };
+
+            queuebtn.Click += (s, e) => {
+                System.Diagnostics.Debug.WriteLine("queue clicked");
+                Toast.MakeText(ApplicationContext, "queue clicked", ToastLength.Short).Show();
+            };
+
+            playbt.Click += (s, e) => {
+                System.Diagnostics.Debug.WriteLine("bt_play clicked");
+                if (!playing)
+                {
+                    PlayMusic();
+                    playing = true;
+                    playbtn.SetImageResource(Resource.Mipmap.pause);
+                    playbt.SetImageResource(Resource.Mipmap.pause);
+                }
+                else
+                {
+                    PauseMusic();
+                    playing = false;
+                    playbtn.SetImageResource(Resource.Mipmap.playy);
+                    playbt.SetImageResource(Resource.Mipmap.playy);
+                }
+                Toast.MakeText(ApplicationContext, "bt_play clicked", ToastLength.Short).Show();
+            };
+
+            mp.Error += (sender, args) =>
+            {
+                Console.WriteLine("Error in playback resetting: " + args.What);
+                StopMusic();
+            };
+
+            seekbar.ProgressChanged += Seekbar_ProgressChanged;
+            seekbar.StartTrackingTouch += Seekbar_StartTrackingTouch;
+            seekbar.StopTrackingTouch += Seekbar_StopTrackingTouch;
+
+            txtSongName.Text = "Ranjishe hi sahi";
+            txtArtistName.Text = "Papon";
+
+            layout.Click += (s, e) =>
+              {
+                  Toast.MakeText(ApplicationContext, "layout clicked", ToastLength.Short).Show();
+              };
+            layout.AnchorPoint = 0.3f;
+            layout.PanelExpanded += (s, e) => { System.Diagnostics.Debug.WriteLine("PanelExpanded");
+                playbtn.Visibility = Android.Views.ViewStates.Invisible;
+                queuebtn.Visibility = Android.Views.ViewStates.Invisible;
+                txtArtistName.Visibility = Android.Views.ViewStates.Invisible;
+            };
+            layout.PanelCollapsed += (s, e) => { System.Diagnostics.Debug.WriteLine("PanelCollapsed");
+                playbtn.Visibility = Android.Views.ViewStates.Visible;
+                queuebtn.Visibility = Android.Views.ViewStates.Visible;
+                txtArtistName.Visibility = Android.Views.ViewStates.Visible;
+            };
+            layout.PanelAnchored += (s, e) => System.Diagnostics.Debug.WriteLine("PanelAnchored");
+            layout.PanelSlide += (s, e) =>
+            {
+                if (e.SlideOffset < 0.2)
+                {
+                    if (SupportActionBar.IsShowing)
+                    {
+                        SupportActionBar.Hide();
+                    }
+                }
+                else
+                {
+                    if (!SupportActionBar.IsShowing)
+                        SupportActionBar.Show();
+                }
+            };
+
+            var actionBarHidden = bundle != null &&
+                                  bundle.GetBoolean(SavedStateActionBarHidden, false);
+            if (actionBarHidden)
+                SupportActionBar.Hide();                        
+        }
+
+        private void OnItemClick(object sender, int e)
+        {
+            
+        }
+
+        //seekbar events
+        private void updateSeekbar(object sender, BufferingUpdateEventArgs e)
+        {
+            var x = (MediaPlayer)sender;
+            System.Diagnostics.Debug.WriteLine("seeking to : " + x.CurrentPosition + " from " + seekbar.Max);
+            seekbar.SecondaryProgress = (e.Percent * mp.Duration) / 100;
+        }
+        private void Seekbar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+
+        }
+        private void Seekbar_StartTrackingTouch(object sender, SeekBar.StartTrackingTouchEventArgs e)
+        {
+            mHandler.RemoveCallbacks(updateTimeTask);
+        }
+        private void Seekbar_StopTrackingTouch(object sender, SeekBar.StopTrackingTouchEventArgs e)
+        {
+            if (mp != null)
+                if (mp.IsPlaying)
+                    mp.SeekTo(seekbar.Progress);
+        }
+        private void updateProgressBar()
+        {
+            mHandler.PostDelayed(updateTimeTask, 1000);
+        }
+        private void updateTimeTask()
+        {
+            System.Diagnostics.Debug.WriteLine("My timer call ::  seeking the bar to position");
+            if (mp != null)
+            {
+                if (mp.IsPlaying)
+                    if (mp.CurrentPosition < seekbar.Max)
+                    {
+                        seekbar.SetProgress(mp.CurrentPosition, true);
+                        //seekbar_top.SetProgress(mp.CurrentPosition, true);
+                        mHandler.PostDelayed(updateTimeTask, 1000);
+                    }
+            }
+        }
+
+        Handler mHandler = new Handler();
+        private int length = 0;
+        private PhotoAlbum mPhotoAlbum;
+        private RecyclerView mRecyclerView;
+        private RecyclerView.LayoutManager mLayoutManager;
+        private PhotoAlbumAdapter mAdapter;
+
+        //music stop play pause methods
+
+        private void StopMusic()
+        {
+            if (mp != null)
+                if (mp.IsPlaying)
+                    mp.Stop();
+        }
+
+        private void PauseMusic()
+        {
+            if (mp != null)
+                if (mp.IsPlaying)
+                {
+                    mp.Pause();
+                    length = mp.CurrentPosition;
+                }
+        }
+
+        private async void PlayMusic(string mp3 = "http://cdn5.jatt.link/f7861b11c29524fa7c8035af8d4a3847/ssqgv/Ankhon%20Mein%20Teri-(Mr-Jatt.com).mp3")
+        {
+            if (length != 0)
+            {
+                mp.SeekTo(length);
+                mp.Start();
+                updateProgressBar();
+            }
+            else
+            {
+                mp.Reset();
+                await mp.SetDataSourceAsync(ApplicationContext, Android.Net.Uri.Parse(mp3));
+                mp.PrepareAsync();
+            }
+        }
+
+        public override void OnBackPressed()
+        {
+            SlidingUpPanelLayout ss = (SlidingUpPanelLayout)FindViewById(Resource.Id.sliding_layout);
+            if (ss != null && (ss.IsExpanded || ss.IsAnchored))
+            {
+                ss.CollapsePane();                
+            }
+            //else
+            //{
+            //    base.OnBackPressed();
+            //}
+        }
+    }    
+}
+
---
+++ KonkaniMusic/PhotoAlbumAdapter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace KonkaniMusic
+{
+    public class PhotoViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView Image { get; private set; }
+        public TextView Caption { get; private set; }
+
+        // Get references to the views defined in the CardView layout.
+        public PhotoViewHolder(View itemView, Action<int> listener)
+            : base(itemView)
+        {
+            // Locate and cache view references:
+            Image = itemView.FindViewById<ImageView>(Resource.Id.imageView);
+            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
+
+            // Detect user clicks on the item view and report which item
+            // was clicked (by position) to the listener:
+            itemView.Click += (sender, e) => listener(base.Position);
+        }
+    }
+    internal class PhotoAlbumAdapter: RecyclerView.Adapter
+    {
+        // Event handler for item clicks:
+        public event EventHandler<int> ItemClick;
+
+        // Underlying data set (a photo album):
+        public PhotoAlbum mPhotoAlbum;
+
+        // Load the adapter with the data set (photo album) at construction time:
+        public PhotoAlbumAdapter(PhotoAlbum photoAlbum)
+        {
+            mPhotoAlbum = photoAlbum;
+        }
+
+        // Create a new photo CardView (invoked by the layout manager): 
+        public override RecyclerView.ViewHolder
+            OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            // Inflate the CardView for the photo:
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.PhotoCardView, parent, false);
+
+            // Create a ViewHolder to find and hold these view references, and 
+            // register OnClick with the view holder:
+            PhotoViewHolder vh = new PhotoViewHolder(itemView, OnClick);
+            return vh;
+        }
+
+        // Fill in the contents of the photo card (invoked by the layout manager):
+        public override void
+            OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            PhotoViewHolder vh = holder as PhotoViewHolder;
+
+            // Set the ImageView and TextView in this ViewHolder's CardView 
+            // from this position in the photo album:
+            vh.Image.SetImageResource(mPhotoAlbum[position].PhotoID);
+            vh.Caption.Text = mPhotoAlbum[position].Caption;
+        }
+
+        // Return the number of photos available in the photo album:
+        public override int ItemCount
+        {
+            get { return mPhotoAlbum.NumPhotos; }
+        }
+
+        // Raise an event when the item-click takes place:
+        void OnClick(int position)
+        {
+            if (ItemClick != null)
+                ItemClick(this, position);
+        }
+    }
+}
